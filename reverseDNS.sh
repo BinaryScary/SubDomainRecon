1c454b6e53de436e7d2c049da9b00746f9853abc
Samuel Wong <32050386+BinaryScary@users.noreply.github.com>
2020-03-11 16:51:21 -0400

    Add files via upload

---
+++ reverseDNS.sh
@@ -0,0 +1,14 @@
+if [ $# -lt 1 ]; then
+    echo "[!] Usage: SubDomainRecon.sh [ip-list]"
+    exit 1
+fi
+
+while read ip; do
+	if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+		buf=$(host "$ip" | awk '{ print $5 }' | sed ' s/\.$//g')
+		if [ $buf != "3(NXDOMAIN)" ]
+		then
+			echo "$buf"
+		fi
+	fi
+done < $1
